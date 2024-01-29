f18c2b6750ec976c4e9f4d7a272bc91c6919eea5
gadgelogger <sarayou273142910@gmail.com>
2024-01-29 22:42:10 +0900

    Initial Commit

---
+++ MyJanken/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyJanken
+//
+//  Created by gadgelogger on 2024/01/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
